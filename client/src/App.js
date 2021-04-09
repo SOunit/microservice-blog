66d8e3c7aa23d01a4dba1ad8e48a19d38fcde481
SOUnit <workbookwork7@gmail.com>
2021-04-09 15:06:24 +0900

    added ,in react, http request to post

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='container'>
+      <h1>CreatePost</h1>
+      <PostCreate />
+    </div>
+  );
 };
 
 export default App;
