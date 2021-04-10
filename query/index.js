ab953a3ada6bc9de689c1775a91ca8fbcbb191c6
SOUnit <workbookwork7@gmail.com>
2021-04-10 11:09:59 +0900

    added event parsing incoming events

--- query/index.js
+++ query/index.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
-app.post('/events', (req, res) => {});
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  console.log('Event Received:', type);
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log('Listening on 4002');
